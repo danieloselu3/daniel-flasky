4034b05b2202ceba0e382e90fb4dd252ad8f97c5
danieloselu3 <doselu3@gmail.com>
2019-01-19 13:58:50 +0300

    modified hello.py and base.html

--- dan_flasky/hello.py
+++ dan_flasky/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -14,7 +14,7 @@ moment = Moment(app)
 
 
 class NameForm(FlaskForm):
-    name = StringField('Firstname', validators=[DataRequired()])
+    name = StringField('what is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 @app.errorhandler(404)
@@ -27,12 +27,15 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
         form.name.data = ''
-    return render_template('index.html',form=form, name=name)
+        return redirect(url_for('index'))
+    return render_template('index.html',form=form, name=session.get('name'))
 
 if __name__ == '__main__':
     app.run(debug = True)
